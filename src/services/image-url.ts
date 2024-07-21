e98bf1f3454905fc4bcd0323f6eaad72ce63bb14
Abdul G Zziwa <143699732+abduzziwa@users.noreply.github.com>
2024-07-21 17:56:55 +0200

    Get Optimised images.

---
+++ src/services/image-url.ts
@@ -0,0 +1,19 @@
+
+
+// const getCropperImageUrl = (url: string) => {
+//     const target = '/media/';
+//     const index = url.indexOf('target') + target.length;
+//     console.log(url);
+//     console.log(url.slice(0, index) + 'crop/600/400/' + url.slice(index));
+//     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+// }
+
+// export default getCropperImageUrl;
+const getCropperImageUrl = (url: string) => {
+    const target = '/media/';
+    const index = url.indexOf(target) + target.length;
+    const newUrl = url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+    return newUrl;
+}
+
+export default getCropperImageUrl;
